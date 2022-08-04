4de9827f723744ed9217ca3badccb5bc10dbb3ff
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-04 11:17:39 +0430

    handleClick function to displaying last state

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,34 +1,46 @@
+import { useState } from "react";
+
 const App = () => {
+  const [display, setDisplay] = useState("");
+  const [activities, setActivities] = useState([]);
+  // handle states values
+  const handleClick = (val) => {
+    setDisplay(val);
+    setActivities([...activities, val]);
+  };
+  // calculate function
+
   return (
     <div>
-      <h4>test</h4>
+      {/* last state */}
+      <h4>{display}</h4>
 
       <span>test</span>
       {/* keyboard */}
       <section>
         {/* numbers */}
         <section className="buttons">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
+          <button onClick={() => handleClick(7)}>7</button>
+          <button onClick={() => handleClick(8)}>8</button>
+          <button onClick={() => handleClick(9)}>9</button>
 
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
+          <button onClick={() => handleClick(4)}>4</button>
+          <button onClick={() => handleClick(5)}>5</button>
+          <button onClick={() => handleClick(6)}>6</button>
 
-          <button>3</button>
-          <button>2</button>
-          <button>1</button>
+          <button onClick={() => handleClick(3)}>3</button>
+          <button onClick={() => handleClick(2)}>2</button>
+          <button onClick={() => handleClick(1)}>1</button>
 
-          <button>0</button>
+          <button onClick={() => handleClick(0)}>0</button>
         </section>
         {/* operators */}
         <section className="operators">
-          <button>/</button>
-          <button>*</button>
-          <button>-</button>
-          <button>+</button>
-          <button>=</button>
+          <button onClick={() => handleClick("/")}>/</button>
+          <button onClick={() => handleClick("*")}>*</button>
+          <button onClick={() => handleClick("-")}>-</button>
+          <button onClick={() => handleClick("+")}>+</button>
+          <button onClick={() => handleClick("=")}>=</button>
         </section>
       </section>
     </div>
