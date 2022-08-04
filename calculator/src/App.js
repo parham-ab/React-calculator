99e9934eb93e3a91c20441d4edb4cab951f78927
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-04 11:07:19 +0430

    initial jsx

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,5 +1,38 @@
 const App = () => {
-  return <div>asdasdasd</div>;
+  return (
+    <div>
+      <h4>test</h4>
+
+      <span>test</span>
+      {/* keyboard */}
+      <section>
+        {/* numbers */}
+        <section className="buttons">
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+
+          <button>3</button>
+          <button>2</button>
+          <button>1</button>
+
+          <button>0</button>
+        </section>
+        {/* operators */}
+        <section className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>-</button>
+          <button>+</button>
+          <button>=</button>
+        </section>
+      </section>
+    </div>
+  );
 };
 
 export default App;
